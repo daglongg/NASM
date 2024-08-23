2e55dc2d6135cb6219a356122d8490fb2927b9de
gn01gnad <138242812+daglongg@users.noreply.github.com>
2024-08-23 17:03:28 +0700

    Update Bai_2.asm

--- Challenge/Week1/Challenge_2/Bai_2.asm
+++ Challenge/Week1/Challenge_2/Bai_2.asm
@@ -1,1 +1,47 @@
+section .data
+    mess db 'Enter the text: '
+    lenMess equ $-mess 
+
+    printMess db 'Text your enter is: '
+    lenPrint equ $-printMess
+section .bss
+    userEnter resb 32; resb la 10 byte, tam 2^79
+section .text
+    global _start
+        _start:
+            ; Display mess ask user enter text    
+            mov eax, 4
+            mov ebx, 1
+            mov ecx, mess
+            mov edx, lenMess
+            int 80h
+
+            ; user enter
+            mov eax, 3
+            mov ebx, 1
+            mov ecx, userEnter   
+            mov edx, 32
+            int 80h
+
+            ; Print mess display
+            mov eax, 4
+            mov ebx, 1
+            mov ecx, printMess
+            mov edx, lenPrint
+            int 80h
+
+            ; Print user enter
+            mov eax, 4
+            mov ebx, 1
+            mov ecx, userEnter
+            mov edx, 32
+            int 80h
+            
+            ;Exit program
+            mov eax, 1
+            int 80h
+
+
+
+
 
